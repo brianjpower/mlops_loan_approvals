fa3353a7324da8f7a868177200ce535b3ae7045c
Brian Power <brian.j.power@gmail.com>
2025-09-10 14:32:25 +0100

    Reading loan approval data file into a dataframe

--- bp_load_exp.py
+++ bp_load_exp.py
@@ -1,5 +1,9 @@
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import ModelEncoder
+#from sklearn.preprocessing import ModelEncoder
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
 
+df = pd.read_csv("../loan_approval_dataset.csv")
+print(df.head())

fe39ea553c7700a66801b2129d0ec43018e26ece
Brian Power <brian.j.power@gmail.com>
2025-09-17 12:44:57 +0100

    Updateding the setup.py file to enable the creation of the package files for the project

--- loan-ml-model/setup.py
+++ loan-ml-model/setup.py
@@ -1,14 +1,60 @@
+import io
+from pathlib import Path
+import os
+
 from setuptools import setup, find_packages
 
+#Package Metadata
+NAME = 'loan-ml-model'
+DESCRIPTION = 'A ML package to predict loan outcomes from certain financial data'
+URL = 'https://github.com/brianjpower/mlops_loan_approvals'
+EMAIL = 'brian.j.power@gmail.com'
+AUTHOR = 'Brian Power'
+REQUIRES_PYTHON = '>=3.11.9'
+
+pwd = os.path.abspath(os.path.dirname(__file__))
+
+# Get the list of packages to be installed
+def list_reqs(fname='requirements.txt'):
+    with io.open(os.path.join(pwd, fname), encoding='utf-8') as f:
+        return f.read().splitlines()
+
+try:
+    with io.open(os.path.join(pwd, 'README.md'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+
+# Load the package's __version__.py module as a dictionary.
+ROOT_DIR = Path(__file__).resolve().parent
+PACKAGE_DIR = ROOT_DIR / NAME
+about = {}
+with open(PACKAGE_DIR / 'VERSION') as f:
+    _version = f.read().strip()
+    about['__version__'] = _version
+
 setup(
-    name="loan_mlops",
-    version="0.0.1",
-    author="Brian Power",
-    description="A ML package to predict loan outcomes from certain financial data",
-    packages=find_packages(),
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(exclude=('tests',)),
+    package_data={'prediction_model': ['VERSION']},
+    install_requires=list_reqs(),
+    extras_require={},
+    include_package_data=True,
     classifiers=[
-        "Programming Lanuage :: Python :: 3",
-        "Licence :: OSI approved :: bplic",
-        "Operating System :: OS independent",
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
     ],
 )
